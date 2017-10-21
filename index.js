6605e83b79648816440efb89d7212728f3004e5c
Maxwell DeVos <maxdevos@iastate.edu>
2017-10-20 23:18:51 -0500

    added phaser js files

---
+++ index.js
@@ -0,0 +1,9 @@
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//
+//											ISU Hackathon Game Client 2017											 //
+//								Created By: Max DeVos, Thomas Powell, and Mason Timmerman							 //
+//																													 //
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//
+
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
